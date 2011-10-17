b81b892d5f69becb10c94ac83b875bf3a615069a
Marco Gonzalo Gomez Perez <mgomez@stcsolutions.com.ve>
2011-10-16 19:40:38 -0430

    Proyecto rails base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tukiosquito::Application.config.secret_token = '945910285db2ee8960861e5f698114c15b1e86db9b3256fd103dbffb9f486b34bb36c38a10789ee92435c3d5957ac4f8ec962440078620f33c424b0626d99ce6'
